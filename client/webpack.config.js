13ad1e207c1864975bc9666a7cdb6642e011b8e7
Erik <erikejan@hotmail.com>
2017-01-14 13:43:09 +0100

    Added react hot loader

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,9 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: [
+  	'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
     './src/index.js'
   ],
   module: {
@@ -18,6 +22,10 @@ module.exports = {
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: './public'
-  }
+    contentBase: './public',
+	hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
