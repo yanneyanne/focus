a6f3966ed39a131fe989b34cced2f2f851580815
Erik <erikejan@hotmail.com>
2017-01-14 13:38:42 +0100

    Initial Hello world logging react script

---
+++ client/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/public',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './public'
+  }
+};
