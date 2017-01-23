3e338c6f69b8413addbd007405fffd758d2cbea0
Erik <erikejan@hotmail.com>
2017-01-23 14:53:32 +0100

    Default function returning state

--- client/app/reducer.js
+++ client/app/reducer.js
@@ -1,15 +1,16 @@
 import {List, Map} from 'immutable'
 
-function defaultFunction() {
+function defaultFunction(state) {
 	console.log("Calling default function in reducer")
+	return state
 }
 
 export default function(state = Map(), action) {
 	switch (action.type) {
 		case 'ADDING':
-			return defaultFunction()
+			return defaultFunction(state)
 		case 'DELETE':
-			return defaultFunction()
+			return defaultFunction(state)
 	}
 	return state
 	
