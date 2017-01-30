d6839c74886aec40f7a2ec2e0712997f4b96ca7d
Erik <erikejan@hotmail.com>
2017-01-30 21:48:19 +0100

    Switched to even more minimal setup, enabled .less-styling

--- client/app/components/Header.js
+++ client/app/components/Header.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
-import styles from '../assets/styles/Header.css'
 
 export default class Header extends Component {
 	render() {
 		return (
-			<div className={styles.header}>
+			<div >
 				Logo
 				<button>?</button>
 			</div>
--- client/app/index.js
+++ client/app/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App'
+import React from 'react'
+import {render} from 'react-dom'
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
+import App from './components/App'
 import reducer from './reducer'
+import styles from './styles/main.less';
+
+'use strict';
 
 const store = createStore(reducer)
-
 render(
-	<Provider store={store}>
-		<App />	
+	<Provider store = {store}>
+		<App />
 	</Provider>,
-	document.getElementById('root')
-);
+	document.getElementById('content')
+)
