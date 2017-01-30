3ff5e100cd15212bfd73908b9543b5b9ebf956ce
Erik <erikejan@hotmail.com>
2017-01-30 22:12:01 +0100

    Less style imports now working

--- client/app/components/Header.js
+++ client/app/components/Header.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Header extends Component {
 	render() {
 		return (
-			<div >
+			<div className="Header">
 				Logo
 				<button>?</button>
 			</div>
