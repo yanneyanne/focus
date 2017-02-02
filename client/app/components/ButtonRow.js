66b9d7f9f2bcc3b7bb58edbb2358143f5a3f19c7
Erik <erikejan@hotmail.com>
2017-02-02 11:01:47 +0100

    Added basic Header and ButtonRow .less-styling

--- client/app/components/ButtonRow.js
+++ client/app/components/ButtonRow.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class ButtonRow extends Component {
 	render() {
 		return (
-			<div>
+			<div className="ButtonRow">
 				<button>Block</button>
 			</div>
 		)	
--- client/app/components/Header.js
+++ client/app/components/Header.js
@@ -4,7 +4,7 @@ export default class Header extends Component {
 	render() {
 		return (
 			<div className="Header">
-				Logo
+				<img src="../app/assets/logo_placeholder.png"/>
 				<button>?</button>
 			</div>
 		)	
