47c0354f0a00380612581f88e0e4853fbaa3e992
Erik <erikejan@hotmail.com>
2017-06-04 08:05:39 +0200

    Added basic api-handler on clientside

---
+++ client/app/lib/api.js
@@ -0,0 +1,42 @@
+class Api {
+  static headers() {
+    return {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'dataType': 'json',
+    }
+  }
+
+  static get(route) {
+    return this.xhr(route, null, 'GET');
+  }
+
+  //static put(route, params) {
+  //  return this.xhr(route, params, 'PUT')
+  //}
+
+  static post(route, params) {
+    return this.xhr(route, params, 'POST')
+  }
+
+  static delete(route, params) {
+    return this.xhr(route, params, 'DELETE')
+  }
+
+  static xhr(route, params, verb) {
+    const host = 'http://localhost:5000/'
+    const url = `${host}${route}`
+    let options = Object.assign({ method: verb }, params ? { body: JSON.stringify(params) } : null );
+    options.headers = Api.headers()
+    
+    return fetch(url,options).then((response) => {
+      let json = response.json()
+      if(response.ok) {
+        return json
+      }
+      return json.then(err => {throw err})
+    })
+  }
+}
+
+export default Api
