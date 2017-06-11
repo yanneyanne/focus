524441ba995ec2d823f22c9fab67ae31445021c3
Erik <erikejan@hotmail.com>
2017-06-11 09:27:58 +0200

    Now possible to toggle blocker state via POST request

--- blocker/routes/blocker.py
+++ blocker/routes/blocker.py
@@ -1,9 +1,33 @@
 #!env/bin/python3
-from flask import jsonify, request, make_response
+from flask import jsonify, request, make_response, abort
 from blocker import app
-from tinydb import TinyDB
+from tinydb import TinyDB, Query
 
 db = TinyDB('blocker/blocker_db.json')
-blocker = db.table('blocker_state')
+blocker = db.table('blocker')
 
+@app.route('/blocker', methods = ['GET'])
+def get_blocker():
+    State = Query()
+    blocker_state = blocker.get(State.state != None)
+    if blocker_state is None:
+        blocker_state = 'inactive'
+    return jsonify({'blocker': blocker_state})
+
+@app.route('/blocker', methods=['PUT'])
+def set_blocker_state():
+    if (not request.json or
+        not 'state' in request.json or
+        (request.json['state'] not in ['active', 'inactive'])):
+        abort(400)
+    State = Query()
+    if blocker.contains(State.state != None):
+        blocker.update({'state': request.json['state']}, cond=State.state!=None)
+    else:
+        blocker.insert({'state': request.json['state']})
+    return jsonify({'new_state': request.json['state']})
+
+@app.errorhandler(400)
+def unexpected_payload(error):
+    return make_response(jsonify({'error': 'Expected JSON payload with "state" set to "active" or "inactive"'}), 400)
 
