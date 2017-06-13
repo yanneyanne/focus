826ab9d26546cc0610780fe8aa63fa7969bb2aa5
Erik <erikejan@hotmail.com>
2017-06-13 22:57:57 +0200

    Removed the header and some misc cleanups

--- client/app/components/App.js
+++ client/app/components/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react'
-import Header from './Header'
 import BlockList from './BlockList'
 import Timer from './Timer'
 import ButtonRow from './ButtonRow'
-import {List} from 'immutable'
+import { List } from 'immutable'
 
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Header />
         <BlockList />
         <Timer />
         <ButtonRow />
