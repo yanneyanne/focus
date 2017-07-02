ac0b61323d8f5eca7fdad7539bd174517e3d5e59
Erik <erikejan@hotmail.com>
2017-07-02 21:43:37 +0200

    Fixed time input bug

--- client/app/components/Timer.js
+++ client/app/components/Timer.js
@@ -7,26 +7,30 @@ class Timer extends Component {
   getPlaceholderValue() {
     return "00:00"
   }
-  receivingInput(e) {
-    console.log("Receiving input...")
-    // Sanitize for numeric inputs only
-    if(e.charCode < 48 || e.charCode > 57)
-      e.preventDefault();
+
+  sanitizeInput(e) {
+    // Allow only numeric or backspace input 4 digits long
+    if((e.charCode < 48 || e.charCode > 57) ||
+      e.target.value.length > 4)
+      e.preventDefault()
+  }
+
+  formatTime(e) {
+    var newTime = e.target.value.replace(":", "")
     // Keep the colon at the right place
-    if(e.target.value.length >= 2) {
-      var value = e.target.value.replace(":", "")
-      value = value.substring(0, value.length - 1) + ":" + value.substring(value.length - 1, value.length)
-      e.target.value = value
+    if(newTime.length > 2) {
+      newTime = newTime.substring(0, newTime.length - 2) + ":" + newTime.substring(newTime.length - 2, newTime.length)
     }
+    e.target.value = newTime
   }
 
   render() {
     return (
       <div className="Timer">
         <input
-          onKeyPress={(e) => this.receivingInput(e)}
-          placeholder={this.getPlaceholderValue()}
-          />
+          onKeyPress={(e) => this.sanitizeInput(e)}
+          onChange={(e) => this.formatTime(e)}
+          placeholder={this.getPlaceholderValue()} />
       </div>
     ) 
   }
