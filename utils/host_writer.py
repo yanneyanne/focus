17f5d999d8e0d6f6bbc667e00d805516b41aa2ca
Erik <erikejan@hotmail.com>
2017-08-26 21:48:30 +0200

    Redirecting hosts to localhost

--- utils/host_writer.py
+++ utils/host_writer.py
@@ -1,18 +1,18 @@
 #!env/bin/python3
 
 def block_hosts(new_hosts):
-    with open("/etc/hosts_test", 'r+') as hosts_file:
+    with open("/etc/hosts", 'r+') as hosts_file:
         hosts_list = hosts_file.readlines()
         for new_host in new_hosts:
-            new_host = new_host + "\n"
+            new_host = "127.0.0.1 " + new_host + "\n"
             if new_host not in hosts_list:
                 hosts_file.write(new_host)
 
 def unblock_hosts(hosts):
-    with open("/etc/hosts_test", 'w+') as hosts_file:
+    with open("/etc/hosts", 'w+') as hosts_file:
         hosts_list = hosts_file.readlines()
         for host in hosts:
-            host = host + "\n"
+            host = "127.0.0.1 " + host + "\n"
             try:
                 host_list.remove(host)
                 break
